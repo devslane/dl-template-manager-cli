a3faca8a0b548a60d0f32448ab36ea384bebc334
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-03-08 21:51:54 +0530

    [FIX]

--- index.ts
+++ index.ts
@@ -1,12 +1,32 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
-import frameworkQuestion from './utils/framework';
-import shell from "shelljs"
+import shell from 'shelljs';
+import chalk from 'chalk';
+import { frameworkQuestion, frontendTemplates, projectName } from './utils/options';
+import { FrameworkOptions, FrontendTemplateLinks } from './utils/enums';
 
-shell.echo("Welcome to Devslane Template Manager");
+shell.echo(chalk.green('Welcome to Devslane Template Manager'));
 
-inquirer.prompt(frameworkQuestion).then((answers: any) => {
-    if (answers.framework === 'Backend') {
-        shell.echo("Backend Templates are in progress");
-    }
+inquirer.prompt(projectName).then((pAnswer: any) => {
+    const projectNameA = pAnswer.project_name;
+    inquirer.prompt(frameworkQuestion).then((fAnswers: any) => {
+        if (fAnswers.framework === FrameworkOptions.BACKEND) {
+            shell.echo(chalk.red('Backend Templates are in progress'));
+        }
+        if (fAnswers.framework === FrameworkOptions.FRONTEND) {
+            inquirer.prompt(frontendTemplates).then((fTemplate: any) => {
+                const selectedTemplate = fTemplate.frontend_template;
+                shell.echo(chalk.green(`STARTED : Cloning ${selectedTemplate}`));
+                const cloneCommand = FrontendTemplateLinks[selectedTemplate];
+                if (cloneCommand) {
+                    shell.exec(`npx degit ${cloneCommand} ${projectNameA}`);
+                    shell.echo(chalk.green(`COMPLETE : Cloning ${selectedTemplate}`));
+                    shell.echo(chalk.bgWhite(chalk.blue('Follow these steps to start working')));
+                    shell.echo(chalk.yellow(`1. cd ${projectNameA}`));
+                    shell.echo(chalk.yellow('2. yarn'));
+                    shell.echo(chalk.yellow('3. yarn run dev'));
+                }
+            });
+        }
+    });
 });
---
+++ utils/enums.ts
@@ -0,0 +1,15 @@
+export const FrameworkOptions = {
+    BACKEND: 'Backend',
+    FRONTEND: 'Frontend',
+};
+
+export const FrontendTemplateOptions = {
+    VITE_REACT_TAILWIND_REDUX: 'Vite React Tailwind Redux Boilerplate',
+    NEXT_TAILWIND_REDUX: 'NextJs Tailwind Redux Boilerplate',
+};
+
+export const FrontendTemplateLinks = {
+    [FrontendTemplateOptions.NEXT_TAILWIND_REDUX]: 'https://github.com/manbhasin/next-redux-tailwind-boilerplate',
+    [FrontendTemplateOptions.VITE_REACT_TAILWIND_REDUX]:
+        'https://github.com/manbhasin/vite-react-redux-tailwind-boilerplate',
+};
