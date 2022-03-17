238998b44a241bd351b8d39ec6b2e02eb3042ed9
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-03-17 17:46:56 +0530

    [UPDATE] github url

--- utils/enums.ts
+++ utils/enums.ts
@@ -5,11 +5,13 @@ export const FrameworkOptions = {
 
 export const FrontendTemplateOptions = {
     VITE_REACT_TAILWIND_REDUX: 'Vite React Tailwind Redux Boilerplate',
+    VITE_REACT_TAILWIND: 'Vite React Tailwind Boilerplate',
     NEXT_TAILWIND_REDUX: 'NextJs Tailwind Redux Boilerplate',
 };
 
 export const FrontendTemplateLinks = {
-    [FrontendTemplateOptions.NEXT_TAILWIND_REDUX]: 'https://github.com/manbhasin/next-redux-tailwind-boilerplate',
+    [FrontendTemplateOptions.NEXT_TAILWIND_REDUX]: 'https://github.com/devslane/nextjs-redux-tailwind-boilerplate',
     [FrontendTemplateOptions.VITE_REACT_TAILWIND_REDUX]:
-        'https://github.com/manbhasin/vite-react-redux-tailwind-boilerplate',
+        'https://github.com/devslane/vite-react-redux-tailwind-boilerplate',
+    [FrontendTemplateOptions.VITE_REACT_TAILWIND]: 'https://github.com/devslane/vite-react-tailwind-boilerplate',
 };
