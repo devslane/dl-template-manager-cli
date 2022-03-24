7e2883a03c76ee2709f3cf91427bb29070dd9ca5
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-03-24 12:17:06 +0530

    [UPDATE] commands

--- utils/cloneTailwindUI.command.ts
+++ utils/cloneTailwindUI.command.ts
@@ -3,27 +3,26 @@ import shell from 'shelljs';
 const cloneTailwindUICommands = () => {
     shell.mkdir('src/shared-components');
     shell.cd('src/shared-components');
+
+    // ** Clone Tailwind UI Component Repo
     shell.exec('npx degit https://github.com/devslane/tailwind-react-ui-components temp-repo');
     shell.cd('temp-repo');
 
-    //! TEMP COMMANDS START
-    shell.mkdir('src');
-    shell.mkdir('src/components');
-    shell.mkdir('src/constants');
-    shell.mkdir('src/styles');
-    shell.mkdir('src/hooks');
-    shell.touch(['src/components/test1.txt', 'src/components/test2.txt']);
-    shell.touch(['src/constants/constant1.txt', 'src/constants/constant2.txt']);
-    shell.touch(['src/styles/style1.txt', 'src/styles/style2.txt']);
-    shell.touch(['src/hooks/hooks1.txt', 'src/hooks/hooks2.txt']);
-    //! TEMP COMMANDS END
-
+    // ** Move required folders to the root
     shell.mv('src/components/*', '..');
     shell.mv('src/constants', '..');
     shell.mv('src/styles', '..');
     shell.mv('src/hooks', '../..');
     shell.cd('..');
+
+    // ** Delete unnecessary data
     shell.rm('-r', 'temp-repo/');
+
+    // ** Add required libraries
+    shell.exec('npx sb init');
+    shell.exec('yarn add -D @headlessui/react @heroicons/react');
+    shell.exec('yarn add formik');
+
     shell.cd('..');
 };
 
--- utils/cloneTemplate.command.ts
+++ utils/cloneTemplate.command.ts
@@ -3,9 +3,14 @@ import { FrontendTemplateLinks } from './enums';
 
 const cloneTemplateCommands = (projectName: string, selectedTemplate: string) => {
     const cloneCommand = FrontendTemplateLinks[selectedTemplate];
+    // ** Clone Template from the required repo
     shell.exec(`npx degit ${cloneCommand} ${projectName}`);
     shell.cd(projectName);
+
+    // ** Initialize git
     shell.exec('git init');
+
+    // ** Install dependencies
     shell.exec('yarn');
 };
 
