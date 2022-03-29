a5c5b9281e6be75aa20f89cf49527204d6f7a3a7
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-03-29 13:47:25 +0530

    [FIX] dir structures for copying shared files

--- utils/cloneTailwindUI.command.ts
+++ utils/cloneTailwindUI.command.ts
@@ -1,27 +1,36 @@
+import chalk from 'chalk';
 import shell from 'shelljs';
+import updateTailwindConfig from '../scripts/update-tailwind-config';
 
 const cloneTailwindUICommands = () => {
-    shell.mkdir('src/shared-components');
-    shell.cd('src/shared-components');
+    shell.cd('src');
 
     // ** Clone Tailwind UI Component Repo
+    shell.echo(chalk.green('STARTED: Cloning Shared Components'));
     shell.exec('npx degit https://github.com/devslane/tailwind-react-ui-components temp-repo');
+
     shell.cd('temp-repo');
 
     // ** Move required folders to the root
-    shell.mv('src/components/*', '..');
-    shell.mv('src/constants', '..');
-    shell.mv('src/styles', '..');
-    shell.mv('src/hooks', '../..');
+    shell.mv('src/shared-resources', '..');
+    shell.mv('src/hooks', '..');
+    shell.mv('index.scss', '..');
+    shell.echo(chalk.green('COMPLETE: Cloning Shared Components'));
     shell.cd('..');
 
     // ** Delete unnecessary data
     shell.rm('-r', 'temp-repo/');
 
+    shell.cd('..');
+
     // ** Add required libraries
-    shell.exec('npx sb init');
-    shell.exec('yarn add -D @headlessui/react @heroicons/react');
+    shell.echo(chalk.green('STARTED: Dependencies ADD'));
+    shell.exec('yarn add -D classnames @headlessui/react @heroicons/react');
     shell.exec('yarn add formik');
+
+    shell.echo(chalk.green('COMPLETE: Dependencies ADD'));
+
+    updateTailwindConfig();
 
     shell.cd('..');
 };
